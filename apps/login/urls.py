f2240fe1e453aeeb384482a2b1cd5d37e0d1c3aa
Troy Center <troycenter1@gmail.com>
2017-06-24 14:23:03 -0700

    login app lifted in and tested

---
+++ apps/login/urls.py
@@ -0,0 +1,11 @@
+''' App URLS.py file. '''
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^register$', views.register),
+    url(r'^mainpage$', views.mainpage),
+    url(r'^login$', views.login),
+    url(r'^logout$', views.logout),
+]
